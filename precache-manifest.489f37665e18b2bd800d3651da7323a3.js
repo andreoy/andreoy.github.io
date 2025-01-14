5f646f8f15f0624f52af3aebbd7df7840671e7f3
Andreo <39822719+andreoy@users.noreply.github.com>
2025-01-14 10:19:34 +0700

    Add files via upload

---
+++ precache-manifest.489f37665e18b2bd800d3651da7323a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901da5321e9458936966ed7b220cc39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "313069482d7bde9d842f",
+    "url": "/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "6da06725a51acd908060",
+    "url": "/static/css/main.3522ec90.chunk.css"
+  },
+  {
+    "revision": "313069482d7bde9d842f",
+    "url": "/static/js/2.4a2773d6.chunk.js"
+  },
+  {
+    "revision": "1299ed581ef47dcd8388bb4526f15704",
+    "url": "/static/js/2.4a2773d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89dd3b6d840be2459e4",
+    "url": "/static/js/3.9c4042b1.chunk.js"
+  },
+  {
+    "revision": "6da06725a51acd908060",
+    "url": "/static/js/main.d30861a1.chunk.js"
+  },
+  {
+    "revision": "90ee68b28dbc87a1d392",
+    "url": "/static/js/runtime-main.946476fd.js"
+  }
+]);
