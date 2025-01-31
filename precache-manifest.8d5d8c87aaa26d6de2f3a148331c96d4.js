b8eddd39d8d601cef3192cc8979b31a30075f7f7
Andreo <39822719+andreoy@users.noreply.github.com>
2025-01-31 19:02:04 +0700

    update data baru

---
+++ precache-manifest.8d5d8c87aaa26d6de2f3a148331c96d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e144366668adfc1b7dda6a32301a64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c01e892ec428157dc92",
+    "url": "/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "1ef0246ffcceb11a121a",
+    "url": "/static/css/main.d3a50976.chunk.css"
+  },
+  {
+    "revision": "5c01e892ec428157dc92",
+    "url": "/static/js/2.d54d1c69.chunk.js"
+  },
+  {
+    "revision": "1299ed581ef47dcd8388bb4526f15704",
+    "url": "/static/js/2.d54d1c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0496a7c6108971e7356b",
+    "url": "/static/js/3.9eaa3c41.chunk.js"
+  },
+  {
+    "revision": "1ef0246ffcceb11a121a",
+    "url": "/static/js/main.217a0296.chunk.js"
+  },
+  {
+    "revision": "1a17e46d6e93847a3951",
+    "url": "/static/js/runtime-main.d4e2a973.js"
+  }
+]);
