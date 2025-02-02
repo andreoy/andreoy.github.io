a1e9c4c6c5a96f18d1a798d7bd8d135b9a173d86
Andreo <39822719+andreoy@users.noreply.github.com>
2025-02-02 17:14:40 +0700

    Update mayor

---
+++ precache-manifest.74a33ab4f814fd206e87b6c2d47921e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad743b94868fb395e399342d7ab06c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceeb7d9e05bc367e2934",
+    "url": "/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "813d9033e86a4bf32828",
+    "url": "/static/css/main.198b0607.chunk.css"
+  },
+  {
+    "revision": "ceeb7d9e05bc367e2934",
+    "url": "/static/js/2.0a62356e.chunk.js"
+  },
+  {
+    "revision": "1299ed581ef47dcd8388bb4526f15704",
+    "url": "/static/js/2.0a62356e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71d912b873620755b02",
+    "url": "/static/js/3.0ca1e849.chunk.js"
+  },
+  {
+    "revision": "813d9033e86a4bf32828",
+    "url": "/static/js/main.b85dca4b.chunk.js"
+  },
+  {
+    "revision": "b0d76ee458cc9f0371c0",
+    "url": "/static/js/runtime-main.f2efa484.js"
+  },
+  {
+    "revision": "12243a419eab5f336f689d81b5c0317f",
+    "url": "/static/media/uin-jambi-logo.12243a41.png"
+  }
+]);
